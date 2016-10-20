1ef1a63fb93a1c2e50ffd7c1db52566b773d4199
newbienewbie <webbot@sina.com>
2016-10-20 20:16:20 +0800

    把种子生成变得可配置，为以后大规模爬取做准备

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Crawler=require('./lib/crawler');
+const changzhou=require('./seed/changzhou-50.json');
 
 
 
@@ -9,9 +10,15 @@ const host="http://esf.cz.fang.com";
 // 新建爬虫
 const crawler=new Crawler(host);
 
-// 创建种子
-// house-{区代码}-{镇代码}/{总价区间代码}: 
-const seed=host+'/house-a0342/c280-d2100/';
+
+// 创建种子，种子是一个相对URL
+// 目前总是以/house-开头，完整的格式为：
+// /house-{区代码}-{镇代码}/{总价区间代码}-{分页代码}: 
+// eg:'/house-a0341-b013685/d250/';
+let path=changzhou.children.filter(k=>k.name=="新北")[0].index;
+let condition="d250";
+const seed=host+path+condition;
+console.log(seed);
 
 // 抓取
 crawler.crawl(seed);
