f55845d5615f6c3f5fd45e0679ea0cd4808e620b
newbienewbie <webbot@sina.com>
2016-11-03 21:43:20 +0800

    引入url标准库来合成URL

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Crawler=require('./lib/crawler');
 const changzhou=require('./seed/changzhou-50.json');
+const url=require('url');
 
 
 
 // URL 构成:
 // - esf : 二手房
 // cz : 常州
-const host="http://esf.cz.fang.com";
+const base="http://esf.cz.fang.com";
 // 新建爬虫
-const crawler=new Crawler(host);
+const crawler=new Crawler(base);
 
 
 // 创建种子，种子是一个相对URL
@@ -17,7 +18,8 @@ const crawler=new Crawler(host);
 // eg:'/house-a0341-b013685/d250/';
 let path=changzhou.children.filter(k=>k.name=="新北")[0].index;
 let condition="d250";
-const seed=host+path+condition;
+let seed=url.resolve(base,path);
+seed=url.resolve(seed,condition);
 console.log(seed);
 
 // 抓取
